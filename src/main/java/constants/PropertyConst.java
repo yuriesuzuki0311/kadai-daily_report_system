17cd85213ed3c26b3ebe06f0999432eb2b5aa050
yurie.suzuki <yurie.suzuki@architectdeveloper.com>
2022-09-13 19:33:52 +0900

    定数ファイルを作成

---
+++ src/main/java/constants/PropertyConst.java
@@ -0,0 +1,17 @@
+package constants;
+
+public enum PropertyConst {
+
+  //ペッパー文字列
+    PEPPER("pepper");
+
+    private final String text;
+    private PropertyConst(final String text) {
+        this.text = text;
+    }
+
+    public String getValue() {
+        return this.text;
+    }
+
+}
